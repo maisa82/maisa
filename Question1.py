47330f10357c99ffaeb0aea8063f666909b1188a
maisa82 <70031923+maisa82@users.noreply.github.com>
2020-08-21 13:55:07 -0400

    Add files via upload

---
+++ Question1.py
@@ -0,0 +1,9 @@
+#'''Maisa Allala
+# R00850445
+# This program will write Dark Souls is the best game ever five times'''
+print ("Dark Souls is the best game ever")
+print ("Dark Souls is the best game ever")
+print ("Dark Souls is the best game ever")
+print ("Dark Souls is the best game ever")
+print ("Dark Souls is the best game ever")
+
